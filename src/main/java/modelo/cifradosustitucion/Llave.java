19a1bdef088cd50e0acb1f79c7b5972a9496d812
Raúl Alfaro <125586767+RaJami1205@users.noreply.github.com>
2023-11-24 18:01:05 -0600

    Actualización de datos 3

--- src/main/java/modelo/cifradosustitucion/Llave.java
+++ src/main/java/modelo/cifradosustitucion/Llave.java
@@ -17,35 +17,38 @@ public class Llave extends Criptografia{
    */
   @Override
   public  String encriptar(String texto){
-      texto = texto.toUpperCase();
-      String clave = "TANGO";
-      StringBuilder mensajeCifrado = new StringBuilder();
-      
-      for(int i = 0, j = 0; i < texto.length(); i++) {
-          
-          if (j > clave.length()) {
+    texto = texto.toUpperCase();
+    String clave = "TANGO";
+    StringBuilder mensajeCifrado = new StringBuilder();
+    
+    for(int i = 0, j = 0; i < texto.length(); i++) {
+        
+        if (j >= clave.length()) {
             j = 0;
-          }
-          
-          char letra = texto.charAt(i);
-          char claveLetra = clave.charAt(j);
-          
-          if (Character.isLetter(letra)) {
-             int letraNum = letra - 'A';
-             int claveNum = claveLetra - 'A';
-             
-             int codigo = (letraNum + claveNum) % 26;
-             
-             char letraCifrada = (char) ('A' + codigo);
-             
-             mensajeCifrado.append(letraCifrada);
-             j++;
-             
-          } else {
+        }
+        
+        char letra = texto.charAt(i);
+        char claveLetra = clave.charAt(j);
+        
+        if (Character.isLetter(letra)) {
+            int numLetra = (char) (letra - 'A' + 1) % 26 + 'A';
+            int numClaveLetra = (char) (claveLetra - 'A' + 1) % 26 + 'A';
+            
+            int codigo = numLetra + numClaveLetra; 
+            
+            if (codigo > 26) {
+              codigo = codigo - 26;
+            }
+            char letraCifrada = (char) (codigo % 26 + 'A' - 1); 
+            
+            mensajeCifrado.append(letraCifrada);
+            j++;
+            
+        } else {
             mensajeCifrado.append(letra);
-          }
-      }
-      return mensajeCifrado.toString();
+        }
+    }
+    return mensajeCifrado.toString();
   }
   
   /***
@@ -62,20 +65,23 @@ public class Llave extends Criptografia{
       
       for(int i = 0, j = 0; i < texto.length(); i++) {
           
-          if (j > clave.length()) {
+          if (j >= clave.length()) {
             j = 0;
           }
           
           char letra = texto.charAt(i);
-          char claveLetra = texto.charAt(j);
+          char claveLetra = clave.charAt(j);
           
           if (Character.isLetter(letra)) {
-             int letraNum = letra - 'A';
-             int claveNum = claveLetra - 'A';
+             int letraNum = (letra - 'A' + 1);
+             int claveNum = (claveLetra - 'A' + 1);
+             int codigo = letraNum - claveNum;
              
-             int codigo = (letraNum - claveNum + 26) % 26;
-             
-             char letraDescifrada = (char) ('A' + codigo);
+             if (codigo <= 0) {
+               codigo = codigo + 26;
+             }
+            
+             char letraDescifrada = (char) (codigo % 26 + 'A' - 1); 
              
              mensajeDescifrado.append(letraDescifrada);
              j++;
